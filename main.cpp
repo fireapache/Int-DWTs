51ceebfa912dc7ae72144ea46d92e039d1606830
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2014-09-18 03:12:18 -0300

    [Test3] Main configuration to run test3()

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 
 int main(int argc, char **argv)
 {
-	
+	if (argc > 2)
+	{
+		double p = (double)atoi(argv[2]) / 100.0;
+
+		test3(argv[1], p);
+	}
+	else cout << "Enter with <ppmfilepath> <energytokeep 0-100>" << endl;
 	
 	return 0;
 }
