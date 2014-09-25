6b72be2b4b50c9de91d3bf2afe9f51d88c437858
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2014-09-25 17:10:46 -0300

    [ADDED] Template procedures to print vectors and matrices

--- misc.h
+++ misc.h
@@ -35,4 +35,35 @@ void gnuplot_dat_Wdecomposition(const char *file, double x1, double x2, double *
 void gnuplot_dat_VWdecomposition(const char *file1, const char *file2, double x1, double x2, double *v, int n, int levels, bool normal);
 void data_analysis(double *data, uint n, DataAnalysis *analysis);
 
+template <typename T>
+void printVector(T *vec, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		cout << vec[i] << endl;
+	}
+
+	cout << endl;
+}
+
+template <typename T>
+void printMatrix(T **mat, int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			cout << mat[i] << '\t';
+		}
+
+		cout << endl;
+	}
+
+	cout << endl;
+}
+
 #endif /* MISC_H */
