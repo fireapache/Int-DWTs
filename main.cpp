8aae4f23036ce60e670b46a97a3b32939113f803
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2014-11-07 18:36:41 -0200

    [Test7] Configuration to run test7(...)

--- main.cpp
+++ main.cpp
@@ -4,11 +4,9 @@ int main(int argc, char **argv)
 {
 	if (argc > 1)
 	{
-		double p = (double)atoi(argv[1]) / 100.0;
-
-		test6(p);
+		test7(argv[1]);
 	}
-	else cout << "Enter with <energytocompress 0-100>" << endl;
-	
+	else cout << "Enter with <ppmfilepath>" << endl;
+
 	return 0;
 }
