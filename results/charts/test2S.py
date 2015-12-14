d3a4bcb09fecda1eca9128be5226a85107e7e92f
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2015-12-14 03:02:29 -0200

    Updating chart scripts

--- results/charts/test2S.py
+++ results/charts/test2S.py
@@ -7,12 +7,24 @@ import matplotlib.pyplot as plt
 
 n_groups = 3
 
-fig, ax = plt.subplots(nrows=2, sharex=True)
+# fig, ax = plt.subplots(nrows=3, sharex=True, figsize=(5, 6))
+fig = plt.figure(figsize=(5, 6))
+fig.subplots_adjust(hspace=0.01)
+
+ax = []
+
+ax.append(fig.add_subplot(311))
+ax.append(fig.add_subplot(312, sharex=ax[0]))
+ax.append(fig.add_subplot(337))
+ax.append(fig.add_subplot(338))
+ax.append(fig.add_subplot(339))
 
 index = np.arange(n_groups)
 bar_width = 0.35
 
-opacity = 0.4
+opacity = 1.0
+rcolor = (1.0, 0.6, 0.6)
+bcolor = (0.6, 0.6, 1.0)
 error_config = {'ecolor': '0.3'}
 
 plt.sca(ax[0])
@@ -24,12 +36,13 @@ developed = (0.0443343, 0.0341321, 0.0796046)
 std_dev = (0.00203959, 0.002759, 0.00450897)
 
 plt.ylabel('Time')
-plt.title('Decimated Standard 2D HWT')
+plt.title('Decimated 2D Standard HWT')
 plt.xticks(index + bar_width, ('Decomposition', 'Composition', 'Decomp & Comp'))
+ax[0].xaxis.set_visible(False)
 
 rects1 = ax[0].bar(index, original, bar_width,
                  alpha=opacity,
-                 color='b',
+                 color=bcolor,
                  yerr=std_ori,
                  error_kw=error_config,
                  capsize=6.0,
@@ -37,12 +50,12 @@ rects1 = ax[0].bar(index, original, bar_width,
 
 rects2 = ax[0].bar(index + bar_width, developed, bar_width,
                  alpha=opacity,
-                 color='r',
+                 color=rcolor,
                  yerr=std_dev,
                  error_kw=error_config,
                  label='Developed')
 
-plt.legend(loc='upper left')
+plt.legend(loc='upper left', prop={'size':11})
 
 plt.sca(ax[1])
 
@@ -56,17 +69,94 @@ plt.yscale('log')
 
 rectError1 = ax[1].bar(index, originalError, bar_width,
                  alpha=opacity,
-                 color='b',
+                 color=bcolor,
                  #yerr=std_ori,
                  error_kw=error_config,
                  label='Original')
 
 rectError2 = ax[1].bar(index + bar_width, developedError, bar_width,
                  alpha=opacity,
-                 color='r',
+                 color=rcolor,
                  #yerr=std_ori,
                  error_kw=error_config,
                  label='Developed')
+
+plt.sca(ax[2])
+
+oriEUC = (1.51185e-009)
+devEUC = (2.17089e-012)
+
+plt.ylabel('EUC')
+plt.xticks(np.arange(0))
+plt.ylim([1.000000E-013,7.000000E-008])
+plt.yscale('log')
+
+rectEUC1 = ax[2].bar(1, oriEUC, bar_width,
+                 alpha=opacity,
+                 color=bcolor,
+                 #yerr=std_ori,
+                 #error_kw=error_config,
+                 label='Original')
+
+rectEUC2 = ax[2].bar(1, devEUC, bar_width,
+                 alpha=opacity,
+                 color=rcolor,
+                 #yerr=std_ori,
+                 #error_kw=error_config,
+                 label='Original')
+
+plt.sca(ax[3])
+
+oriMSE = (2.1798e-024)
+devMSE = (4.49444e-030)
+
+ylim = [1.000000E-032,1.000000E-022]
+
+plt.ylabel('MSE')
+plt.xticks(np.arange(0))
+plt.ylim(ylim)
+plt.yscale('log')
+
+rectMSE1 = ax[3].bar(1, oriMSE, bar_width,
+                 alpha=opacity,
+                 color=bcolor,
+                 #yerr=std_ori,
+                 #error_kw=error_config,
+                 label='Original')
+
+rectMSE2 = ax[3].bar(1, devMSE, bar_width,
+                 alpha=opacity,
+                 color=rcolor,
+                 #yerr=std_ori,
+                 #error_kw=error_config,
+                 label='Original')
+
+plt.sca(ax[4])
+
+oriPSNR = (296.822)
+devPSNR = (353.679)
+
+ylim = [270,380]
+
+plt.ylabel('PSNR')
+plt.xticks(np.arange(0))
+plt.ylim(ylim)
+plt.yticks(np.arange(min(ylim), max(ylim), 30.0))
+#plt.yscale('log')
+
+rectPSNR2 = ax[4].bar(1, devPSNR, bar_width,
+                 alpha=opacity,
+                 color=rcolor,
+                 #yerr=std_ori,
+                 #error_kw=error_config,
+                 label='Original')
+
+rectPSNR1 = ax[4].bar(1, oriPSNR, bar_width,
+                 alpha=opacity,
+                 color=bcolor,
+                 #yerr=std_ori,
+                 #error_kw=error_config,
+                 label='Original')
 
 plt.tight_layout()
 plt.show()
